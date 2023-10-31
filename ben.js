5dc59cf6f3ae4672e20cc01c98f35a7ec00dc2de
ben <122939326+beneggen2@users.noreply.github.com>
2023-10-31 11:25:54 -0700

    Add files via upload

---
+++ ben.js
@@ -0,0 +1,157 @@
+_import(manifest.JSON)
+open (getComputedStyle )
+
+caches -a -eval -get -FileList;
+{
+    (k)-fetch -GeolocationPosition == to (URL ());
+    }
+    {
+        if (fetch =(k))false;
+    }
+    console.log(KeyboardEvent ()); URL()
+    {
+     -Request -GeolocationCoordinates -alert -false; 
+       length.toString ; 
+
+    }
+    var toString =function k(fetch) {
+       -CryptoKey -of -URL ();
+        
+
+    }
+    -fetch -CryptoKeyimport== -FileSystemFileHandle;
+    Lock-FileSystemFileHandle;
+{
+ exportMIDIAccess=eval=(r);
+}
+(r)
+--{ 
+    console=log(FileSystemDirectoryHandle)
+    }
+    {get -web -history  -of -StorageEvent () ;
+      -to  -webkitURL ();
+      webkitURL = false }
+    -Lock -FileSystemDirectoryHandle == true
+    {
+      get- KeySystemAccess () ==true;
+     (q); get- KeySystemAccess () ==false;
+      }
+    {
+      console.log(KeySystemAccess);URL()
+      
+    }
+    q;{
+      -Lock -all -File; true
+       -get -seed (); -of -URL()
+       Lock(seed) -localStorage;
+    }
+    -open -FileSystemDirectoryHandle;  {
+        -get -MediaKeySystemAccess ();
+         localStorage
+      
+    }
+     { 
+      -get -MediaKeySession()==true;
+       -open -port (22)==true 
+      
+     }
+     {FileSystemWritableFileStream (true); if (false);
+       w= tryPaymentResponse () = true;
+        d= tryPaymentResponse()-falsereturn (_return (k)) 
+        }
+        k={
+            requestIdleCallbackos: 
+             KeyboardEvent(onclose -window )
+              -true_return(j)
+
+        }
+        -fetch (i)
+        {
+          i  -document (cookies)
+           cache (File) 
+           
+        }
+        _export (i);
+         {
+          var URL=""
+          }
+          get- WebGLProgram- {
+             _RTCDtlsTransport:  
+              get -https (URL -"")
+               -ServiceWorkerRegistration (FileSystemHandle ()  )
+             -KeyPermissionStatus ()
+          }
+           w={ defaultSystemAccess: true 
+           -sessionStorage (caches -cookies -pass )}
+           d={
+               tor:(package) 
+               -__import (tor -package) 
+               -__run (package) 
+           }
+           <br>
+           {
+           fetchdebugger  :package 
+            -onstorage () -caches
+              -defaultSystemAccess()
+           }
+           {
+             encodeURI- "" ; _in (128-B)
+             -get -seed ()
+             } 
+             {
+               EventTarget ( URLSearchParams );
+                -Location (File)
+                  request (JSON) -of -FileSystem -""
+                   
+             }
+                {
+                  -get -PublicKey -of -URL()
+                } 
+                -open (Proxy = URL()  )
+                {
+ fetch- CacheStorage () ;
+                     MIDIConnectionEvent ();
+                     MIDIAccess(caches);
+                     MIDIPort();
+                     
+                }
+                {
+                 DataView (caches);
+                 ifData (package);  [true]
+                 -alert -console.log;
+                }
+                {
+                  fetch()=  CryptoKey  (DataTransferItemList)
+                     ; _default- get (key -package)
+                       if (false) return(q)
+                }
+                (q)={ 
+                  EventSource,  KeyPermissionStatus ,package :
+                  sessionStorage()
+                  }
+                  <br>
+                  {
+                    fetch ,if (true_return) {
+                      FileSystemWritableFileStream 
+                    }
+                  ,true_return, WebGLActiveInfo 
+                  ,LocationWebGLProgram 
+                  ,WebSocket
+                  }
+                   get-toString 
+                   {
+                    DataTransfer ;localStorage
+                    history-caches
+                    fetch- devicePixelRatio ()
+                  devicePixelRatio(4-3 -16-10   )=true
+                   return(F) }
+                  (F)= {
+                         alert :URL () 
+                        -importScripts -of [  URL ()  ]
+                        -run -importScripts ()
+                   }        
+                   {
+                     setTimeout (false)
+                    }
+                    
+  

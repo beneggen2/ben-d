8bd930e8f812ac689e05af98f09fd494562d9939
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-01 11:33:14 -0700

    Add files via upload

---
+++ ben1.js
@@ -0,0 +1,39 @@
+run- importScripts - url 
+import(os)
+import(PublicKeyCredential) ,_if=false -{
+    fetch_PublicKeyCredential
+}
+ PublicKeyCredential= {
+     FileSystemEntry
+     ,PaymentRequesthistory 
+      :alert 
+      
+
+ } 
+ <br>
+ (fetch) ;{ 
+    URLSearchParams
+     :PaymentRequest ()
+    -FileSystemEntry ()
+    -WebGLProgram ()
+     -history -FileList ()
+     
+    } 
+    MIDIPort() = {
+      ValidityState 
+      :ClipboardEvent()
+       .RTCPeerConnection()  
+        
+
+    }
+    MIDIConnectionEvent()={
+       CredentialsContainer 
+        :connection- usb() 
+        
+    }
+    DataView()={
+     ssl :[package] 
+         
+
+    }
+    

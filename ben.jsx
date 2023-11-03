dd0975ccab16b88d30fea5a625e1f5dff01773ea
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-03 09:42:04 -0700

    Add files via upload

---
+++ ben.jsx
@@ -0,0 +1,28 @@
+fetch - ip()
+webkitURL() =
+    importScripts - of - url() 
+run(importScripts)
+{
+    addEventListener = URL -[]
+}
+
+
+send(log) -to -url-[]
+  log = {
+      KeyboardEvent = url - []
+      - DataTransferItem()
+      - usb(localStorage)
+      - MediaKeySession()
+      - Audio()
+}
+{
+    open(WebSocket)
+    WebSocket = (self - send - email())
+    Request(FileSystemDirectoryEntry - key() ) 
+}
+{
+
+
+
+
+}

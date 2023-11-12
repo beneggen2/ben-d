601b42d39ee680dd3f98fcc82cd1a2fa804288cc
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-12 16:51:07 -0700

    Add files via upload

---
+++ ben3.js
@@ -0,0 +1,21 @@
+ open () -Option -FileList()
+ {
+   PublicKeyCredential ()= fetch -root -File()
+    open- AnalyserNode (File_import )
+     debugger ;root -File()
+      _import () -WebSocket ()
+      -importScripts -WebSocket()
+     }
+     open- new seed -key  
+     open_seed- key ()
+     
+    encode-root () -- 
+   encodeURI in (129-B )
+   {
+    get - encode  -key () ;Storage- Location (url) 
+    
+
+   }     
+
+
+ 

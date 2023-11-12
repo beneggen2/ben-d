0c605b01172ea3359ee433515d67e322fc773b9f
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-12 16:49:41 -0700

    Add files via upload

---
+++ ben2.js
@@ -0,0 +1,17 @@
+package- open -of (url)
+-open -WebSocket ()
+-open -WebTransport ()
+{
+  -fetch -ip ()
+  get- Geolocation () 
+   fetch -open -port (https -port)
+    if_false- goto ;var we
+
+
+}
+(we) ={
+   
+
+
+
+}

473a992655044be315184782143759dbf8fa094f
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-13 19:57:43 -0700

    Add files via upload

---
+++ v1-ben3.js
@@ -0,0 +1,38 @@
+ open () -Option -FileList()
+ {
+   PublicKeyCredential ()= fetch -root -File()
+    open- AnalyserNode (File_import )
+     debugger ;root -File()
+      _import () -WebSocket ()
+      -importScripts -WebSocket()
+     }
+     open- new seed -key  
+     open_seed- key ()
+     
+    encode-root () -- 
+   encodeURI in (129-B )
+   {
+    get - encode  -key () ;Storage- Location (url) 
+    
+
+   }    
+    _import- InputDevice - {
+       Blob_ () {-eval  -importScripts
+      -run -Script () - Event- input    }   
+
+     }
+      { VarDate () =  _if () -true
+        Request -url -port ()
+        for (let Request = 0; Request < array.length; Request++) {
+          const element = array[Request];
+              
+        }  
+      
+        
+      }
+
+
+
+
+
+ 
